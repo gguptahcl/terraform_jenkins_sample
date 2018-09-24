0c0307651144ee3c877b650c1d819588a656b555
Gaurav gupta <g.gupta@hcl.com>
2018-09-24 16:03:58 -0700

    updated vars

--- vars.tf
+++ vars.tf
@@ -1,3 +1,7 @@
-variable "ACCESS_KEY" {}
+variable "ACCESS_KEY" { 
+	#default="AKIAINLP6VORZCZZVMMA"
+}
 
-variable "SECRET_KEY" {}
+variable "SECRET_KEY" {
+	#default="C6VZeDILPxQhlcOLa5mGa0ocINippm6OIzzgi0m5"
+}
